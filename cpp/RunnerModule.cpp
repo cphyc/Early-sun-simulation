e9d3445121121b322887b0257ef1408949f8a940
cphyc <admin@cphyc.me>
2014-06-06 15:26:47 -0700

    Remove depency in <array>

--- cpp/RunnerModule.cpp
+++ cpp/RunnerModule.cpp
@@ -81,7 +81,7 @@ void coeff(double Omega, double dlnOmegadlnr, double kR, double kZ,
 
 // Loop between Omega_range[om_b] & Omega_range[om_t] 
 // (resp dom_b & dom_t for dlnOmegadlnr)
-void loop(arr FGMs, double Omega_range[], double dlnOmegadlnr_range[],
+void loop(double FGMs[NOMEGA][NDOMEGA], double Omega_range[], double dlnOmegadlnr_range[],
 	  double k_range[], int om_b, int om_e,
 	  int dom_b, int dom_e, int nk) {
   
--- cpp/RunnerModule.hpp
+++ cpp/RunnerModule.hpp
@@ -1,13 +1,12 @@
-#include <array>
-const int N = 100;
-typedef std::array< std::array<double, N>, N > arr;
+#include "simul_constants.hpp"
 
 // Compute a single FGM
 double get_FGM(double, double);
 
 // Loop between Omega_range[om_b] & Omega_range[om_t] 
 // (resp dom_b & dom_t for dlnOmegadlnr)
-void loop(arr FGMs, double Omega_range[], double dlnOmegadlnr_range[],
+void loop(double FGMs[NOMEGA][NDOMEGA], double Omega_range[],
+	  double dlnOmegadlnr_range[],
 	  double k_range[], int om_b, int om_e,
 	  int dom_b, int dom_e, int nk);
 
